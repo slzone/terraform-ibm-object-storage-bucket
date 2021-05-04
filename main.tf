93b259d81491eddd70e530963ddbf7eefc1b4268
shalkygaur <82538063+shalkygaur@users.noreply.github.com>
2021-05-04 10:49:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ data "ibm_resource_instance" "cos_instance" {
     resource_group_id = data.ibm_resource_group.resource_group.id
     name              = "slz-rg-cos"
     service           = "cloud-object-storage"
-    #plan             = var.plan
     location          = var.resource_location
 }
 
@@ -23,7 +22,6 @@ data "ibm_resource_instance" "cos_instance" {
 data "ibm_resource_instance" "hpcs_instance" {
     resource_group_id = data.ibm_resource_group.resource_group.id
     name              = var.hpcs_instance_name
-    #service          = var.hpcs_instance_name
 }
 
 # KMS key managed by HPCS
@@ -42,17 +40,10 @@ resource "ibm_cos_bucket" "bucket_instance" {
   count                   = (var.provision ? 1 : 0)
   bucket_name             = local.bucket_name
   resource_instance_id    = data.ibm_resource_instance.cos_instance.id
-  cross_region_location   = var.region
+  region_location         = var.region_location
   storage_class           = var.storage_class
-  key_protect             = data.ibm_kms_key.kmskey.id
+  key_protect             = data.ibm_kms_key.kmskey.keys[0].crn
+}
 }
 
-/*** resource "ibm_iam_authorization_policy" "policy" {
-  #resource_group_id            = data.ibm_resource_group.rg_name.id
-  depends_on                    = [ibm_cos_bucket.bucket_instance]
-  source_service_name           = "cloud-object-storage"
-  #source_resource_instance_id  = ibm_resource_instance.cos_instance.id
-  target_service_name           = "hpcs"
-  #target_resource_instance_id  = ibm_resource_instance.hpcs_instance.id
-  roles                         = ["Reader"]
-} ***/
+
